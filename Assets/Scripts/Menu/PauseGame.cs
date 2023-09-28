2e5a93f81cfc803f8c4f48094e4851d61374f257
Boto2512 <danwilson2512@gmail.com>
2023-09-28 14:55:20 +0100

    Pause Function & TMPro
    Made Pause game feature and added TMPro to the project

--- Assets/Scripts/Menu/PauseGame.cs
+++ Assets/Scripts/Menu/PauseGame.cs
@@ -1,24 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PauseGame : MonoBehaviour
 {
-    public Text textPaused;
-    [SerializeField] private bool isPaused = false;
+    public TextMeshProUGUI textPaused;
+    [SerializeField] private bool isPaused;
+    [SerializeField] private bool isMainMenu;
 
 
     // Start is called before the first frame update
     void Start()
     {
         textPaused.enabled = false;
+
+        Scene currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name != "MainMenu")
+        {
+            isMainMenu = false;
+        } else if (currentScene.name == "MainMenu")
+        {
+            isMainMenu = true;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape)) 
+        if(Input.GetKeyDown(KeyCode.P) ) 
         {
             Pause();
         }
@@ -26,15 +38,19 @@ public class PauseGame : MonoBehaviour
 
     private void Pause()
     {
-        if (!isPaused) 
+        if (!isPaused)
         {
+            print("Is Paused");
             textPaused.enabled = true;
             Time.timeScale = 0f;
+            isPaused = true;
         }
         else if (isPaused)
         {
+            print("Not Paused");
             textPaused.enabled = false;
             Time.timeScale += 1f;
+            isPaused = false;
         }
     }
 }
