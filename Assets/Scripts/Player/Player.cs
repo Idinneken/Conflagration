5fd0a68884da9953a338afc22756b6cfc415c157
GEORGE NORMAN <2003314@uad.ac.uk>
2023-09-29 17:30:57 +0100

    Player can increase scale + power

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,27 +1,75 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float startingPoints = 0;
+    private float initialPoints = 0;
     public float points;
-    public int flameIntensity;
 
+    public PlayerData playerData;
+    private int initialPlayerDataIndex = 0;
+    public int playerDataIndex = 0;
+
+    public int flameIntensity;
+    public float playerScale;
+    public float cameraDistance;
     public float buttonMashForce;
+
+    private static List<PlayerData> intensityScalePlayerData = new()
+    {
+        { new PlayerData { flameIntensity = 0, playerScale = 1f, cameraDistance = 10f, buttonMashForce = 1f } },
+        { new PlayerData { flameIntensity = 1, playerScale = 2f, cameraDistance = 15f, buttonMashForce = 1.5f } },
+        { new PlayerData { flameIntensity = 2, playerScale = 5f, cameraDistance = 20f, buttonMashForce = 2f } },
+        { new PlayerData { flameIntensity = 3, playerScale = 10f, cameraDistance = 30f, buttonMashForce = 3f } },
+    };
+
+    public ThirdPersonFreeCam thirdPersonFreeCam;
 
     public void Start()
     {
-        points = startingPoints; 
+        points = initialPoints;
+        playerDataIndex = initialPlayerDataIndex;
+        UpdatePlayerData(intensityScalePlayerData[initialPlayerDataIndex]);
     }
 
     public void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            IncreaseIntensity();
+        }
     }
 
     public void AlterPointsLevel(float quantity)
     {
         points += quantity;
     }
+
+    public void IncreaseIntensity()
+    {
+        playerDataIndex++;
+        if (playerDataIndex <= intensityScalePlayerData.Count)
+        {
+            UpdatePlayerData(intensityScalePlayerData[playerDataIndex]);
+
+            gameObject.transform.localScale = new Vector3(playerScale, playerScale, playerScale);
+            thirdPersonFreeCam.distance = cameraDistance;
+        }
+    }
+
+    public void UpdatePlayerData(PlayerData playerData)
+    {
+        this.playerData = playerData;
+        playerScale = playerData.playerScale;
+        cameraDistance = playerData.cameraDistance;
+        buttonMashForce = playerData.buttonMashForce;
+    }
+}
+
+public struct PlayerData
+{
+    public int flameIntensity;
+    public float playerScale;
+    public float cameraDistance;
+    public float buttonMashForce;
 }
